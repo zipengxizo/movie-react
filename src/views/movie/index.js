42382d081e13b9b85aa0e6f1bb05d2e687809c53
zipengxiao <929822043@qq.com>
2020-11-13 16:13:08 +0800

    格式化

--- src/views/movie/index.js
+++ src/views/movie/index.js
@@ -1,37 +1,38 @@
 import React from "react";
-import { Route, Redirect } from "react-router-dom";
-import CacheRoute, { CacheSwitch } from "react-router-cache-route";
+import {Route, Redirect} from "react-router-dom";
+import CacheRoute, {CacheSwitch} from "react-router-cache-route";
 import Header from "@/component/header";
 import "./index.css";
 import Menu from "@/component/menu";
 import loadable from '@/util/loadable'
-const Research = loadable(()=>import('@/component/research'));
-const City = loadable(()=>import('@/component/city'));
-const NowPlaying = loadable(()=>import('@/component/nowPlaying'));
+
+const Research = loadable(() => import('@/component/research'));
+const City = loadable(() => import('@/component/city'));
+const NowPlaying = loadable(() => import('@/component/nowPlaying'));
 export default class Movie extends React.Component {
-  render() {
-    return (
-      <div id="main">
-        <Header title="电影" />
-        <div id="content">
-          <Menu></Menu>
-          <CacheSwitch>
-            <CacheRoute path="/movie/city" component={City}></CacheRoute>
-            <CacheRoute
-              path="/movie/nowPlaying"
-              component={NowPlaying}
-            ></CacheRoute>
-            <CacheRoute
-              path="/movie/search"
-              component={Research}
-              className="search_box"
-            ></CacheRoute>
-            <Route path="/">
-              <Redirect to="/movie/nowPlaying" />
-            </Route>
-          </CacheSwitch>
-        </div>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div id="main">
+                <Header title="电影"/>
+                <div id="content">
+                    <Menu></Menu>
+                    <CacheSwitch>
+                        <CacheRoute path="/movie/city" component={City}/>
+                        <CacheRoute
+                            path="/movie/nowPlaying"
+                            component={NowPlaying}
+                        />
+                        <CacheRoute
+                            path="/movie/search"
+                            component={Research}
+                            className="search_box"
+                        />
+                        <Route path="/">
+                            <Redirect to="/movie/nowPlaying"/>
+                        </Route>
+                    </CacheSwitch>
+                </div>
+            </div>
+        );
+    }
 }
